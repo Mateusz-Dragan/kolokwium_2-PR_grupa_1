7e9b59e46ef271a24947c62493c270ef5d3ce712
Mateusz <mateuszek1029@gmail.com>
2021-06-11 10:37:33 +0200

    Kolokwium_Gr_1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { Route, Switch, Redirect } from "react-router-dom";
 import Posts from "./components/posts";
+import Home from "./components/MateuszDraganhome"
+import NavBar from "./components/MateuszDragannavbar";
 
 function App() {
 
   return (
       <div className="container-fluid">
+          <NavBar />
         <div className="container">
             <div className="content">
                 <Switch>
-                    <Route path="/" exact component={Posts} />
+                    <Route path="/home" component={Home} />
+                    <Route path="/posts" exact component={Posts} />
                 </Switch>
             </div>
         </div>
